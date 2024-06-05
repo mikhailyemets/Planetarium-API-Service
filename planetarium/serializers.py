d9a23058b5170e86ce7b3fdb2862f5eff103e7e7
Mykhailo Yemets <mikhailyemets.pl@gmail.com>
2024-06-05 18:49:29 +0200

    add Astronomy List/Retrieve end-point

--- planetarium/serializers.py
+++ planetarium/serializers.py
@@ -20,3 +20,23 @@ class AstronomyShowSerializer(serializers.ModelSerializer):
     class Meta:
         model = AstronomyShow
         fields = ("id", "title", "description", "theme")
+
+
+class AstronomyShowListSerializer(serializers.ModelSerializer):
+    theme = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field="name"
+    )
+
+    class Meta:
+        model = AstronomyShow
+        fields = ("id", "title", "description", "theme")
+
+
+class AstronomyShowDetailSerializer(serializers.ModelSerializer):
+    theme = ShowThemeSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = AstronomyShow
+        fields = ("id", "title", "description", "theme")
--- planetarium/views.py
+++ planetarium/views.py
@@ -11,7 +11,9 @@ from planetarium.models import (
 from rest_framework import viewsets
 from planetarium.serializers import (
     ShowThemeSerializer,
-    AstronomyShowSerializer
+    AstronomyShowSerializer,
+    AstronomyShowListSerializer,
+    AstronomyShowDetailSerializer
 )
 
 
@@ -24,3 +26,11 @@ class AstronomyShowViewSet(viewsets.ModelViewSet):
     queryset = AstronomyShow.objects.prefetch_related("theme")
     serializer_class = AstronomyShowSerializer
 
+    def get_serializer_class(self):
+        if self.action == "list":
+            return AstronomyShowListSerializer
+
+        if self.action == "retrieve":
+            return AstronomyShowDetailSerializer
+
+        return super().get_serializer_class()
